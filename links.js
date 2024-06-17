69ca0379fbde42c955bb48aba094c41f21ef266e
Jose Enrico Bandong <pianocoderodyssey@Joses-MBP.toshima.ne.jp>
2024-06-17 23:13:55 +0900

    updated tonight yet again

--- links.js
+++ links.js
@@ -1,32 +1,50 @@
 export const links = [
   {
-    name: "Instagram",
-    link: "https://www.instagram.com/tokiochouetsu/",
-    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-  },
-  {
-    name: "Moon Reprise - Terminal 2 Recordings SoundCloud",
-    link: "https://soundcloud.com/terminal2recordings/sets/low-velocity-society-moon-reprise",
-    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-  },
-  {
-    name: "Holy Human Waters - Terminal 2 Recordings SoundCloud",
-    link: "https://soundcloud.com/terminal2recordings/sets/low-velocity-society-holy-human-waters",
-    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-  },
-  {
-    name: "A Different Way - Terminal 2 Recordings SoundCloud",
-    link: "https://soundcloud.com/terminal2recordings/sets/low-velocity-society-a-different-way",
-    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-  },
-  {
-    name: "Moon Reprise Sheet Music - SheetMusicPlus",
+    name: "Moon Reprise Sheet Music",
+    name2: "SheetMusicPlus",
     link: "https://www.sheetmusicplus.com/en/product/moon-reprise-22787429.html",
     image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
   },
   {
-    name: "Moon Reprise Sheet Music - SheetMusicDirect",
+    name: "Moon Reprise Sheet Music",
+    name2: "SheetMusicDirect",
     link: "https://www.sheetmusicdirect.com/en-US/se/ID_No/1559705/Product.aspx",
+    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+  },
+  {
+    name: "Moon Reprise",
+    name2: "Terminal 2 Recordings SoundCloud",
+    link: "https://soundcloud.com/terminal2recordings/sets/low-velocity-society-moon-reprise",
+    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+  },
+  {
+    name: "Moon Reprise",
+    name2: "Terminal 2 Recordings Bandcamp",
+    link: "https://terminal2recordings.bandcamp.com/album/moon-reprise-two-track-single",
+    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+  },
+  {
+    name: "Moon Reprise",
+    name2: "Terminal 2 Recordings YouTube",
+    link: "https://www.youtube.com/watch?v=qRAh7nHs-c4",
+    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+  },
+  {
+    name: "Holy Human Waters",
+    name2: "Terminal 2 Recordings SoundCloud",
+    link: "https://soundcloud.com/terminal2recordings/sets/low-velocity-society-holy-human-waters",
+    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+  },
+  {
+    name: "A Different Way",
+    name2: "Terminal 2 Recordings SoundCloud",
+    link: "https://soundcloud.com/terminal2recordings/sets/low-velocity-society-a-different-way",
+    image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+  },
+  {
+    name: "Instagram",
+    name2: "",
+    link: "https://www.instagram.com/tokiochouetsu/",
     image: "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
   },
 ];
--- main.js
+++ main.js
@@ -2,17 +2,20 @@ import { links } from "./links.js";
 
 const linkContainer = document.getElementById("links");
 
-function addLink(name, link, image) {
+function addLink(name, name2, link, image) {
   const textOne = ` 
     <a href="${ link }" class="link" target="blank"> 
       <img src="${ image }"/> 
-      <span>${ name }</span> 
+      <span>${ name }</span>
       <img class="linkIcon" src="link-solid.svg" alt=""/> 
     </a> 
   `
   const textTwo = ` 
-    <a href="${ link }" class="link" target="blank"> 
-      <span>${ name }</span> 
+    <a href="${ link }" class="link" target="blank">
+      <div class="names">
+        <span class="nameOne">${ name }</span>
+        <span class="nameTwo">${ name2 }</span>
+      </div> 
     </a> 
   `
 
@@ -24,9 +27,10 @@ let allLinks = "";
 links.forEach((ele) => {
     let link = ele.link;
     let name = ele.name;
+    let name2 = ele.name2;
     let image = ele.image;
 
-    allLinks += addLink(name, link, image);
+    allLinks += addLink(name, name2, link, image);
 });
 
 linkContainer.innerHTML = allLinks;
